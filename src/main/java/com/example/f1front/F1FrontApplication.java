f989fdc7aafad18c04695e1c2a2f4a8aef401b08
Marcel <chlastawamarcel@gmail.com>
2023-11-20 20:04:51 +0100

    1

---
+++ src/main/java/com/example/f1front/F1FrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.f1front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1FrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1FrontApplication.class, args);
+    }
+
+}
